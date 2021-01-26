c2448672fc5957c71675735578f30d9d3c276513
sta143 <sadjad_tavakoli@sfu.ca>
2021-01-25 16:54:46 -0800

    2

--- ExprOps.cpp
+++ ExprOps.cpp
@@ -1,8 +1,10 @@
-// 1f3f0cf7a91b9d72bf734d3a2e74a655
+﻿// 1f3f0cf7a91b9d72bf734d3a2e74a655
 
 #include "ExprTree.h"
 #include "ExprOps.h"
 #include <cassert>
+#include <string>
+#include <iostream>
 
 // You will want to implement these functions within this file.
 
@@ -15,10 +17,14 @@ using exprtree::OpCode;
 using exprtree::Symbol;
   
 namespace exprtree {
-std::optional<int64_t>
 
-evaluate(const ExprTree& tree, const Environment& environment) {
-  return {};
+
+std::optional<int64_t>
+evaluate(const ExprTree& tree, const Environment& environment) {	
+
+	EvaluateVisitor evalute_visitor(environment);
+	tree.accept(evalute_visitor);
+	return {};
 }
 
 
@@ -35,3 +41,40 @@ countOps(const ExprTree& tree) {
 
 
 }
+
+
+class EvaluateVisitor : public ExprVisitor {
+
+	public:         
+		Environment environment;  
+		EvaluateVisitor(Environment environment) { // Constructor with parameters
+			environment = environment;
+		}
+
+private:
+	std::optional<int64_t> value;
+	virtual void visitImpl(const Literal& literal) {
+		value = literal.value;
+	}
+	virtual void visitImpl(const Symbol& symbol) { 
+		std::string name = symbol.name;
+		value = environment.get(name);
+	}
+
+	virtual void visitImpl(const Operation& operation) { 
+		const OpCode opCode = operation.opCode;
+		operation.rhs.accept(*this);
+		std::optional<int64_t> rhs_value = *value;
+		std::cout << *value;
+		operation.lhs.accept(*this);
+		std::cout << *value;
+		std::optional<int64_t> lhs_value = *value;		
+
+		std::cout << opCode;
+			// بچه راستیش 
+			// بچه چپش
+			// Return نتیجه
+		
+	}
+};
+
